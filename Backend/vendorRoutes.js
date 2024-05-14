c187e8975df2ee66dc533fef92ffb92061af4937
NaveenBupati <navinbhupathi22@gmail.com>
2024-05-14 16:52:03 +0530

    uploading files from local to github

---
+++ Backend/vendorRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/VendorController.js")
+const auth = require("../middleware.js")
+
+
+router.post("/register",controller.vendorRegistration)
+
+router.get("/getDetails",controller.getDetails)
+
+router.post("/login",controller.login)
+
+
+module.exports = router
